6e7a4d53e68416e8e1f09198775cbb4fb0f2c0ba
Sulav <sulavshrestha023@gmail.com>
2022-06-23 21:20:58 +0545

    project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//navbar
+import { Navbar } from "./components/Navbar";
+
+//pages
+import { About } from "./pages/About";
+import { Error } from "./pages/Error";
+import { Home } from "./pages/Home";
+import { SingleCocktail } from "./pages/SingleCocktail";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
